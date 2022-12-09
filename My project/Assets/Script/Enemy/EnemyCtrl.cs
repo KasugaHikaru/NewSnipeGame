fa15e0a8848db4f3771596e034086f7391125005
a <a>
2022-12-09 16:24:51 +0900

    敵の沸き作る

--- My project/Assets/Script/Enemy/EnemyCtrl.cs
+++ My project/Assets/Script/Enemy/EnemyCtrl.cs
@@ -4,19 +4,36 @@ using UnityEngine;
 
 public class EnemyCtrl : MonoBehaviour
 {
-    [SerializeField] private GameObject[] enemy;
-    [SerializeField] private GameObject[] enemySpornPosi;
+    [SerializeField] private int weakEnemyValue;
+    [SerializeField] private int strongEnemyValue;
+
+    [SerializeField] private GameObject[] allWeakEnemyList;
+    [SerializeField] private GameObject[] allStrongEnemyList;
+
+    private List<GameObject> weakenemyList;
+    private List<GameObject> strongenemyList;
+
+    [SerializeField] private GameObject[] allEnemySpornPosi;
+    private List<GameObject> enemySpornPosi;
+
+    [SerializeField] private float EnemySpornRate;
+    private float EnemySpornRateNum;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
     }
 
+    
+
+    public void EnemySporn()
+    {
+
+    }
 }
