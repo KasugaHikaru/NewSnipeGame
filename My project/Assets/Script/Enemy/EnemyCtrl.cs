4b10f9a6977ccaddfef33592ac8ae57f128d8f04
KasugaHikaru <21010004kh@edu.fca.ac.jp>
2022-12-12 17:50:36 +0900

    敵の沸きをとりあえずつくた

--- My project/Assets/Script/Enemy/EnemyCtrl.cs
+++ My project/Assets/Script/Enemy/EnemyCtrl.cs
@@ -4,14 +4,19 @@ using UnityEngine;
 
 public class EnemyCtrl : MonoBehaviour
 {
-    [SerializeField] private int weakEnemyValue;
-    [SerializeField] private int strongEnemyValue;
+    [SerializeField] private int initWeakEnemyValu;
+    [SerializeField] private int initStrongEnemyValu;
+    [SerializeField] private int initSpornPosiValu;
+
+    private int nowWeakEnemyValu;
+    private int nowStrongEnemyValu;
+    private int nowSpornPosiValu;
 
     [SerializeField] private GameObject[] allWeakEnemyList;
     [SerializeField] private GameObject[] allStrongEnemyList;
 
-    private List<GameObject> weakenemyList;
-    private List<GameObject> strongenemyList;
+    private List<GameObject> weakEnemyList;
+    private List<GameObject> strongEnemyList;
 
     [SerializeField] private GameObject[] allEnemySpornPosi;
     private List<GameObject> enemySpornPosi;
@@ -19,21 +24,88 @@ public class EnemyCtrl : MonoBehaviour
     [SerializeField] private float EnemySpornRate;
     private float EnemySpornRateNum;
 
+    private int   wave;
+    [SerializeField] private int fnishWave;
+    private bool  waveStart;
+
 
     // Start is called before the first frame update
     void Start()
     {
+        Init();
     }
 
     // Update is called once per frame
     void Update()
     {
+        Updata();
     }
 
-    
-
-    public void EnemySporn()
+    public void Init()
     {
+        nowWeakEnemyValu   = initWeakEnemyValu;
+        nowStrongEnemyValu = initStrongEnemyValu;
+        nowSpornPosiValu   = initSpornPosiValu;
 
+        weakEnemyList   = null;
+        strongEnemyList = null;
+        enemySpornPosi  = null;
+
+        EnemySpornRateNum = 0;
+
+        wave = 0;
+        waveStart = true;
+    }
+
+    public void Updata()
+    {
+        if (waveStart)
+        {
+            weakEnemyList   = ListSet_(nowWeakEnemyValu, allWeakEnemyList);
+            strongEnemyList = ListSet_(nowStrongEnemyValu, allStrongEnemyList);
+            enemySpornPosi  = ListSet_(nowSpornPosiValu, allEnemySpornPosi);
+            wave++;
+            waveStart = false;
+        }
+
+        EnemySpornRateNum += Time.deltaTime;
+        
+        if (EnemySpornRateNum >= EnemySpornRate)
+        {
+            EnemySporn(weakEnemyList);
+            EnemySpornRateNum = 0;
+        }
+
+    }
+
+    public List<GameObject> ListSet_(int times, GameObject[] allLsit)
+    {
+        List<GameObject> list = new List<GameObject>();
+
+        for (int i = 0; i < times; i++)
+        {
+            int randomValu;
+            randomValu = Random.Range(0, allLsit.Length);
+            if (!list.Contains(allLsit[randomValu]))
+            {
+                list.Add(allLsit[randomValu]);
+            }
+            else
+            {
+                i--;
+            }
+        }
+        return list;
+    }
+
+    public void EnemySporn(List<GameObject> enemyList)
+    {
+        int enemyTypeValu;
+        enemyTypeValu = Random.Range(0, enemyList.Count);
+
+        int spronPosiValu;
+        spronPosiValu = Random.Range(0, enemySpornPosi.Count);
+
+        Instantiate(enemyList[enemyTypeValu], enemySpornPosi[spronPosiValu].transform);
     }
 }
