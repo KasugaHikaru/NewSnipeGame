9acce652e5501dc4ead0d8ed7bae74025b5bd0cd
KasugaHikaru <21010004kh@edu.fca.ac.jp>
2023-01-07 03:34:22 +0900

    ルートマップ制作中:オブジェクトの見た目だけ完成、次回スクリプト

---
+++ My project/Assets/Script/Screen/RouteMap.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RouteMap : MonoBehaviour
+{
+
+    [SerializeField] private GameObject[] areaObj;
+
+    private const int areaMax = 10;
+    private const int choiceAreaMax = 3;
+
+    private int nowArea;
+    private int nowChoiceArea;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void CreateArea()
+    {
+
+    } 
+
+}
